80429e5b744d328a5f00f4a50146bbf2501adde4
Thiago Medeiros <thiago@tuta.io>
2023-02-21 03:39:50 -0300

    Tablet portrait done

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,12 @@ const menuBtn = document.querySelector('.menu-icon');       // Menu button
 const menu = document.querySelector('.menu');               // Menu nav tag
 const closeBtn = document.querySelector('.close-menu');     // Close button
 
-// Displays the menu when clicking the menu button by adding the 'open' class
+// Displays menu when clicking the hamburger button by adding the 'open' class
 menuBtn.addEventListener('click', function() {
   menu.classList.add('open');
 });
 
-// Hides the menu when clicking the close button by removing the 'open' class
+// Hides menu when clicking the close button by removing the 'open' class
 closeBtn.addEventListener('click', function() {
   menu.classList.remove('open');
 });
